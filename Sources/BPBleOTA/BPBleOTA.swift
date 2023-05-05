7267a06f828a278d5cf727bdcf000fe13e7adea1
zhouxingfa <zhxf2005307@163.com>
2023-05-05 14:23:00 +0800

    初步实现编译脚本

--- Sources/BPBleOTA/BPBleOTA.swift
+++ Sources/BPBleOTA/BPBleOTA.swift
@@ -1,4 +1,4 @@
-public struct BPBleOTA {
+public struct BPBleOTAService {
     public private(set) var text = "Hello, World!"
 
     public init() {
--- Tests/BPBleOTATests/BPBleOTATests.swift
+++ Tests/BPBleOTATests/BPBleOTATests.swift
@@ -6,6 +6,6 @@ final class BPBleOTATests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BPBleOTA().text, "Hello, World!")
+        XCTAssertEqual(BPBleOTAService().text, "Hello, World!")
     }
 }
