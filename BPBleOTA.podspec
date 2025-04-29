c59ef8590fa291ead08e3657cb17eb35506b020a
zhouxingfa <zhxf2005307@163.com>
2025-04-30 00:55:10 +0800

    修正一处依赖库版本的声明

--- BPBleOTA.podspec
+++ BPBleOTA.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'BPBleOTA'
-  s.version          = '0.7.0'
+  s.version          = '0.7.1'
   s.summary          = 'a swift library aggregating NordicDFU and SMPDFU implement'
 
 # This description is used to generate tags and improve search results.
--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BPBleOTA",
-    platforms: [.iOS(.v12),.macOS(.v10_14)],
+    platforms: [.iOS(.v15),.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager.git",
-                 from:"1.6.0"
+                 exact:"1.6.0"
                 ),
         
         .package(
